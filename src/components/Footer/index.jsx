5af9a8e7b55b16e838b5c268e89390f2642c8488
iyamujoshua <joshamu20@gmail.com>
2024-07-03 23:28:56 +0100

    first commit

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -12,8 +12,8 @@ export default function Footer() {
 
       </div>
       <div className="Socials">
-    <a href="https://x.com/CABIBI_SOL" className="parents"><img src={twitter} height={22} width={22} alt="" /></a>
-    <a href="https://t.me/cabibi_sol" className="parents"><img src={telegram} height={22} width={22} alt="" /></a>
+    <a href="#" className="parents"><img src={twitter} height={22} width={22} alt="" /></a>
+    <a href="#" className="parents"><img src={telegram} height={22} width={22} alt="" /></a>
    
 
 </div>
