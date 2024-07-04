e044e2960124af1be3a5b92f7179e509e3fcd4ac
iyamujoshua <joshamu20@gmail.com>
2024-07-04 03:51:50 +0100

    hey

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -14,7 +14,7 @@ const Header = () => {
     <div>
    <div className='Big-Header'>
     <h1>SPARKYONSOL</h1>
-    <p>Sparky, an American Golden Retriever, lived in luxury. He donned a superhero suit and became sparky <br /> launching $sparky to make crypto fun. Sparky's viral antics quickly spread, revolutionizing the crypto space.</p>
+    <p>Join this 14-year-old American Dachshund as he journeys through the Solana ecosystem. <br /> Watch as this curious and tech-savvy pup explores decentralized finance <br /> (DeFi), interacts with smart contracts, and learns about the intricacies of blockchain technology. <br /> From staking tokens to engaging in yield farming, this Dachshund's adventure is both educational and entertaining. <br /> Along the way, he meets various developers, learns about the security features of Solana, and discovers the potential of NFTs and the metaverse. <br /> This journey is a unique blend of canine curiosity and cutting-edge technology, <br /> perfect for enthusiasts of all ages!</p>
     <p className='CA'>CUAMZHziMo3VazR2tKKpDdgtur3b8JLCQZhXcanXpump</p>
     <a href="https://pump.fun/CUAMZHziMo3VazR2tKKpDdgtur3b8JLCQZhXcanXpump" className='Buy_sol'>Buy Now</a>
    </div>
